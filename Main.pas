dbaa57f90ba8edfb69a1d627b6b79220f27dabf9
luxidea <contact@luxidea.net>
2017-02-04 19:25:14 +0900

    begin

---
+++ Main.pas
@@ -0,0 +1,165 @@
+﻿unit Main;
+
+interface //####################################################################
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  System.Math.Vectors,
+  FMX.Types3D, FMX.Objects3D, FMX.Controls.Presentation,
+  FMX.ScrollBox, FMX.Memo, FMX.Controls3D, FMX.Viewport3D, FMX.TabControl,
+  LUX, LUX.FMX, LUX.FMX.Context.DX11,
+  LIB.Material;
+
+type
+  TForm1 = class(TForm)
+    TabControl1: TTabControl;
+      TabItemV: TTabItem;
+        Viewport3D1: TViewport3D;
+          Dummy1: TDummy;
+            Dummy2: TDummy;
+              Camera1: TCamera;
+          Light1: TLight;
+          Grid3D1: TGrid3D;
+          StrokeCube1: TStrokeCube;
+          Dummy3: TDummy;
+            RoundCube1: TRoundCube;
+        Timer1: TTimer;
+      TabItemS: TTabItem;
+        TabControlS: TTabControl;
+          TabItemSV: TTabItem;
+            MemoSV: TMemo;
+          TabItemSP: TTabItem;
+            MemoSP: TMemo;
+    procedure FormCreate(Sender: TObject);
+    procedure Viewport3D1MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
+    procedure Viewport3D1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Single);
+    procedure Viewport3D1MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { private 宣言 }
+    _MouseS :TShiftState;
+    _MouseP :TPointF;
+  public
+    { public 宣言 }
+    _MyMaterial :TMyMaterialSource;
+    ///// メソッド
+    procedure MakeTexture3D;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation //###############################################################
+
+{$R *.fmx}
+
+uses System.Math,
+     LUX.D3, LUX.FMX.Types3D;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+procedure TForm1.MakeTexture3D;
+var
+   X, Y, Z :Integer;
+   P :TSingle3D;
+   C :TAlphaColorF;
+begin
+     with _MyMaterial.Texture3D as TTexture3DBGRA do
+     begin
+          Width  := 128;
+          Height := 128;
+          Depth  := 128;
+
+          for Z := 0 to Depth-1 do
+          begin
+               P.Z := ( Z / Depth - 0.5 ) * 10;
+
+               for Y := 0 to Height-1 do
+               begin
+                    P.Y := ( Y / Height - 0.5 ) * 10;
+
+                    for X := 0 to Width-1 do
+                    begin
+                         P.X := ( X / Width - 0.5 ) * 10;
+
+                         C.R := ( 1 + Cos( 3 * Pi2 * Roo2( Pow2( P.Y ) + Pow2( P.Z ) ) ) ) / 2;
+                         C.G := ( 1 + Cos( 1 * Pi2 * Roo2( Pow2( P.X ) + Pow2( P.Y ) ) ) ) / 2;
+                         C.B := ( 1 + Cos( 2 * Pi2 * Roo2( Pow2( P.Z ) + Pow2( P.X ) ) ) ) / 2;
+
+                         Pixels[ X, Y, Z ] := C.ToAlphaColor;
+                    end;
+               end;
+          end;
+
+          UpdateTexture;
+     end;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+     Assert( Viewport3D1.Context.ClassName = 'TLuxDX11Context', 'TLuxDX11Context クラスが適用されていません。' );
+
+     //////////
+
+     _MyMaterial := TMyMaterialSource.Create( Self );
+
+     with RoundCube1 do
+     begin
+          LocalMatrix    := TMatrix3D.CreateRotationZ( DegToRad( 45 ) )
+                          * TMatrix3D.CreateRotationX( ArcTan( Roo2( 1/2 ) ) );
+
+          MaterialSource := _MyMaterial;
+     end;
+
+     with _MyMaterial do
+     begin
+          ShaderV.Source.Text := MemoSV.Text;
+          ShaderP.Source.Text := MemoSP.Text;
+     end;
+
+     MakeTexture3D;
+end;
+
+//------------------------------------------------------------------------------
+
+procedure TForm1.Viewport3D1MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
+begin
+     _MouseS := Shift;
+     _MouseP := TPointF.Create( X, Y );
+end;
+
+procedure TForm1.Viewport3D1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Single);
+var
+   P :TPointF;
+begin
+     if ssLeft in _MouseS then
+     begin
+          P := TPointF.Create( X, Y );
+
+          with Dummy1.RotationAngle do Y := Y + ( P.X - _MouseP.X ) / 2;
+          with Dummy2.RotationAngle do X := X - ( P.Y - _MouseP.Y ) / 2;
+
+          _MouseP := P;
+     end;
+end;
+
+procedure TForm1.Viewport3D1MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
+begin
+     Viewport3D1MouseMove( Sender, Shift, X, Y );
+
+     _MouseS := [];
+end;
+
+//------------------------------------------------------------------------------
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+begin
+     with Dummy3.RotationAngle do Y := Y + 1;
+end;
+
+end. //#########################################################################
---
+++ _LIBRARY/LIB.Material.pas
@@ -0,0 +1,151 @@
+﻿unit LIB.Material;
+
+interface //#################################################################### ■
+
+uses System.Classes,
+     FMX.Types3D,
+     LUX, LUX.FMX.Material, LUX.FMX.Types3D;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TMyMaterial
+
+     TMyMaterial = class( TLuxMaterial )
+     private
+     protected
+       _FMatrixMVP :TShaderVarMatrix3D;
+       _FMatrixMV  :TShaderVarMatrix3D;
+       _TIMatrixMV :TShaderVarMatrix3D;
+       _Light      :TShaderVarLight;
+       _EyePos     :TShaderVarVector3D;
+       _Opacity    :TShaderVarSingle;
+       _Texture3D  :TShaderVarTexture3D;
+       ///// メソッド
+       procedure DoApply( const Context_:TContext3D ); override;
+     public
+       constructor Create; override;
+       destructor Destroy; override;
+       ///// プロパティ
+       property Texture3D :TShaderVarTexture3D read _Texture3D;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TMyMaterialSource
+
+     TMyMaterialSource = class( TLuxMaterialSource<TMyMaterial> )
+     private
+     protected
+       ///// アクセス
+       function GetTexture3D :TTexture3D;
+     public
+       ///// プロパティ
+       property Texture3D :TTexture3D read GetTexture3D;
+     end;
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+implementation //############################################################### ■
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TMyMaterial
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TMyMaterial.DoApply( const Context_:TContext3D );
+begin
+     inherited;
+
+     with Context_ do
+     begin
+          SetShaders( _ShaderV.Shader, _ShaderP.Shader );
+
+          _FMatrixMVP.Value := CurrentModelViewProjectionMatrix;
+          _FMatrixMV .Value := CurrentMatrix;
+          _TIMatrixMV.Value := CurrentMatrix.Inverse.Transpose;
+          _Light     .Value := Lights[ 0 ];
+          _EyePos    .Value := CurrentCameraInvMatrix.M[ 3 ];
+          _Opacity   .Value := CurrentOpacity;
+     end;
+
+     _ShaderV.SendVars( Context_ );
+     _ShaderP.SendVars( Context_ );
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TMyMaterial.Create;
+begin
+     inherited;
+
+     _FMatrixMVP := TShaderVarMatrix3D .Create( '_FMatrixMVP' );
+     _FMatrixMV  := TShaderVarMatrix3D .Create( '_FMatrixMV'  );
+     _TIMatrixMV := TShaderVarMatrix3D .Create( '_IMatrixMV'  );
+     _Light      := TShaderVarLight    .Create( '_Light'      );
+     _EyePos     := TShaderVarVector3D .Create( '_EyePos'     );
+     _Opacity    := TShaderVarSingle   .Create( '_Opacity'    );
+     _Texture3D  := TShaderVarTexture3D.Create( '_Texture3D'  );
+
+     _ShaderV.Vars := [ _FMatrixMVP,
+                        _FMatrixMV ,
+                        _TIMatrixMV ];
+
+     _ShaderP.Vars := [ _FMatrixMVP,
+                        _FMatrixMV ,
+                        _TIMatrixMV,
+                        _Light     ,
+                        _EyePos    ,
+                        _Opacity   ,
+                        _Texture3D  ];
+end;
+
+destructor TMyMaterial.Destroy;
+begin
+     _FMatrixMVP.Free;
+     _FMatrixMV .Free;
+     _TIMatrixMV.Free;
+     _Light     .Free;
+     _EyePos    .Free;
+     _Opacity   .Free;
+     _Texture3D .Free;
+
+     inherited;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TMyMaterialSource
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+function TMyMaterialSource.GetTexture3D :TTexture3D;
+begin
+     Result := _Material.Texture3D.Value;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+//############################################################################## □
+
+initialization //######################################################## 初期化
+
+finalization //########################################################## 最終化
+
+end. //######################################################################### ■
